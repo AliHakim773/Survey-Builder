e4f9fb92d265e7ec5de499c1796657ed10cdee21
AliHakim773 <alihakim7732@gmail.com>
2023-12-16 22:47:25 +0200

    added register function

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -1,3 +1,28 @@
+const Role = require("../models/role.model")
+const User = require("../models/user.model")
+
 const register = async (rep, res) => {
-    const { username, password, fistName, lastName, age } = rep.body
+    const { username, password, firstName, lastName } = rep.body
+    if (!username || !password || !firstName || !lastName) {
+        res.status(400).send({ message: "Something is missing" })
+    }
+
+    try {
+        const role = await Role.findOne({ name: "user" })
+        const user = await User.create({
+            username,
+            password,
+            firstName,
+            lastName,
+            role: role._id,
+        })
+
+        res.status(200).send({ user })
+    } catch (e) {
+        res.status(500).send({ error: e })
+    }
+}
+
+module.exports = {
+    register,
 }
--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,10 @@ const { connectToMongoDB } = require("./configs/db.configs")
 const app = express()
 app.use(express.json())
 
+//auth routes
+const authRoutes = require("./routes/auth.routes")
+app.use("/auth", authRoutes)
+
 app.listen(8000, () => {
     console.log("Server listining on PORT: ", 8000)
     connectToMongoDB()
