847e9390d39414ee5753def518780b20bd0ae307
AliHakim773 <alihakim7732@gmail.com>
2023-12-16 12:50:34 +0200

    added user model

---
+++ Backend/models/user.model.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            minlength: 3,
+            maxlength: 20,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 6,
+        },
+        firstName: {
+            type: String,
+            required: true,
+            minlength: 2,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            minlength: 2,
+        },
+        age: {
+            type: Number,
+            required: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+userSchema.pre("save", async function (next) {
+    try {
+        const salt = await bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password, salt)
+        next()
+    } catch (error) {
+        console.log("Error in Hasing Password")
+        next(error)
+    }
+})
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = User
